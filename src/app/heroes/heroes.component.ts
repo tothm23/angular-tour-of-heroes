f8d59e913ec0edf80916a979f2d4143afecebfd4
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-20 19:50:46 +0200

    Add a hero property

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-
+  hero = 'Windstorm';
 }
