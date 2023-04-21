2401b0eece963840bc32b0168115e696046df720
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-21 18:32:30 +0200

    Add the @Input() hero property

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
