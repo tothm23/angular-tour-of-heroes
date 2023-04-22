c1d39691830ef7571ddb30f31288a8879a0f4ae0
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-22 19:35:08 +0200

    Send a message from HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,6 +21,7 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
