cc2bab0c14e1c7fe22ff285c1cd8702577963598
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-28 19:07:14 +0200

    Add the dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 // Determines the path and its component
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 // Initialize the metadata and starts it listening for browser location changes
 @NgModule({
