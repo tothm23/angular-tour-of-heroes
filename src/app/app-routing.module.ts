b5cac304f1cb8a58d0c23ecfcb244ca14ce6ccd0
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-28 18:50:55 +0200

    Add the AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+// Determines the path and its component
+const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
 
+// Initialize the metadata and starts it listening for browser location changes
 @NgModule({
+  // Configures the routes variable in one step
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+  // Makes it available to the entire application
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
