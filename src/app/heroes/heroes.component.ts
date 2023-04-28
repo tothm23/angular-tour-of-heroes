30a946043488b9a1c7678b6fc3c2aa30354aafc1
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-28 19:40:21 +0200

    HeroesComponent hero links

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,13 +9,12 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
 
   constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
+    private heroService: HeroService /*private messageService: MessageService*/
   ) {}
 
   /*
@@ -24,16 +23,18 @@ export class HeroesComponent {
     this.heroes = this.heroService.getHeroes();
   }
   */
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+  /*
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+  */
 }
