6f86f535127e833a4c2796f4dbf9315162c95314
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-28 19:50:34 +0200

    Extract the id route parameter

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
 import { ActivatedRoute } from '@angular/router';
@@ -10,7 +10,7 @@ import { HeroService } from '../hero.service';
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css'],
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
 
   constructor(
@@ -23,4 +23,14 @@ export class HeroDetailComponent {
     // An Angular service for interacting with the browser
     private location: Location
   ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    // Gets id from URL id param
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
 }
