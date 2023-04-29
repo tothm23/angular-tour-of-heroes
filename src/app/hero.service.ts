5347def500fc479825b31d3f82d238bd643a0588
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-29 09:48:10 +0200

    Heroes and HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,11 +6,19 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  // URL to web api
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
   /*
   Synchronous
@@ -29,5 +37,10 @@ export class HeroService {
     const hero = HEROES.find((h) => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  // Log a HeroService message with the MessageService
+  private log(messae: string): void {
+    this.messageService.add(`HeroService: ${messae}`);
   }
 }
