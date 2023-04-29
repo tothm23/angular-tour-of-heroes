4e5777cbb9f2fe5d71b5e554e8b7716587a11899
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-29 10:57:41 +0200

    Get hero by id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -66,10 +66,20 @@ export class HeroService {
     };
   }
 
+  // GET hero by id. Will 404 if id not found
   getHero(id: number): Observable<Hero> {
+    /*
     const hero = HEROES.find((h) => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+    */
+
+    // Create URL with the heros id
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /** Log a HeroService message with the MessageService */
