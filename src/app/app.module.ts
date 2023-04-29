e14453332947302762036ef987fb2cce32527579
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-29 09:43:08 +0200

    Simulate a data server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,25 @@ import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+// import { InMemoryDataService } from './in-memory-data.service';
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    // Takes an InMemoryDataService class that primes the in-memory database.
+    /*
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+    */
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
